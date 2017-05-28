88421438a31e73bdf004bf572bba8ed3181dfb92
Mehul Gawde <mehulgawdenxt@gmail.com>
2017-05-28 23:12:12 +0530

    Implementing Form validation and flash messages
    We used email and basic form validation for undefined.

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -11,13 +12,14 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ValidateService } from './services/validate.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent},
-  { path: 'profile', component: ProfileComponent}
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent }
 ]
 
 @NgModule({
@@ -35,9 +37,10 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular/src/app/components/register/register.component.ts
+++ angular/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+  constructor(
+    private flashMessage: FlashMessagesService,
+    private validateService: ValidateService) { }
 
   ngOnInit() {
   }
+  onRegisterSubmit() {
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
 
+    //Req fields
+    if (!this.validateService.validateRegister(user)) {
+      this.flashMessage.show('pls fill all fields', { cssClass: 'alert-danger', timeout: 3000 });
+      return false;
+    }
+    //Req fields
+    if (!this.validateService.validateEmail(user.email)) {
+      this.flashMessage.show('pls use valid email', { cssClass: 'alert-danger', timeout: 3000 });
+      return false;
+    }
+  }
 }
